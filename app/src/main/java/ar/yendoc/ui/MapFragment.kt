616d1d0d1680bfac90e3e1a20c8e322e6fb114fa
Carolina DAlfonso <carolina.dalfonso@gmail.com>
2021-10-18 21:30:08 -0300

    MapFragment

--- app/src/main/java/ar/yendoc/ui/MapFragment.kt
+++ app/src/main/java/ar/yendoc/ui/MapFragment.kt
@@ -1,7 +1,9 @@
 package ar.yendoc.ui
 
+import android.Manifest
 import androidx.fragment.app.Fragment
 import android.R
+import android.content.pm.PackageManager
 
 import android.os.Bundle
 
@@ -9,18 +11,49 @@ import android.view.ViewGroup
 
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.app.ActivityCompat
 import ar.yendoc.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationChangeListener
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener
 
 
-class MapFragment : Fragment() {
+
+
+
+class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+    private var fusedLocationClient: FusedLocationProviderClient? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.activity)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
     }
 }
