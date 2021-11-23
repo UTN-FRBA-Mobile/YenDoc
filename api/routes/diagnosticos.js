99fc8ced68df4601677635ed6b22ba078baddd90
Julian Roman <julianr@unitech.com.ar>
2021-11-23 18:58:11 -0300

    Update api routes

---
+++ api/routes/diagnosticos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {sqlite} = require('../database/sqlite');
+const router = express.Router();
+
+router.route('/')
+    .get(async (req, res, next) => {
+        sqlite.all("SELECT * FROM diagnosticos", [], (err, rows) => {
+            if(err) {
+                res.status(400).json({"error": err.message});
+                throw err;
+            }
+            res.status(200).json({"message": "Success", "data": rows });
+        });
+    });
+
+module.exports = router;
