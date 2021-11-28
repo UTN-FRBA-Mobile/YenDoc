7dff552bb8aa552884aead8169df1b0710eb602f
Carolina DAlfonso <carolina.dalfonso@gmail.com>
2021-11-28 17:12:17 -0300

    PhotosFragment

--- app/src/main/java/ar/yendoc/ui/PhotosFragment.kt
+++ app/src/main/java/ar/yendoc/ui/PhotosFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import androidx.fragment.app.Fragment
 import ar.yendoc.databinding.FragmentPhotosBinding
 
@@ -16,6 +17,12 @@ class PhotosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentPhotosBinding.inflate(inflater, container, false)
+
+
+        val gallery = this.binding.galleryGridView as GridView
+
+
         return binding.root
     }
+
 }
